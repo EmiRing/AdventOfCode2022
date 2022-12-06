a478e86abb724cc3c2511e2e8d7d6877cbcd7e54
Emil Ringwall <emil.ringwall@mardigras.se>
2022-12-06 11:54:45 +0100

    Day 6

---
+++ Day6/Day6.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOFCode2022.Day6
+{
+    public class Day6
+    {
+        private static readonly string Text =
+            File.ReadAllText(@"C:\Emil\dev\AdventOfCode\AdventOFCode2022\InputData\Day6Input.txt");
+
+        public static void Task(int init)
+        {
+            int stringLength = Text.Length;
+            var counter = init;
+
+            while (counter < stringLength)
+            {
+                var evaluatedString = Text.Substring(counter - init, init);
+                var count = evaluatedString.Distinct().Count();
+                if (count == init)
+                {
+                    Console.WriteLine(counter);
+                    break;
+                }
+                counter++;
+            }
+
+            if (counter == stringLength)
+            {
+                Console.WriteLine("Could not find a start-of-message of that length");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@
         {
             // string[] lines = File.ReadAllLines(@"C:\Emil\dev\AdventOfCode\AdventOFCode2022\InputData\Day2Input.txt");
             //Day2.Day2.Task1(lines);
-            Day2.Day2.Task2();
+            Day6.Day6.Task(4);
+            Day6.Day6.Task(14);
+
         }
     }
 }
